e046e43b51282354a534fd80d9974fd86b39c175
xie_s <xie_s@worksap.co.jp>
2013-06-03 14:57:32 +0800

    add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,13 +27,13 @@ class User < ActiveRecord::Base
 	validates :password_confirmation, presence: true
 
 
+		def feed
+		  # This is preliminary. See "Following users" for the full implementation.
+		  Micropost.where("user_id = ?", id)
+		end
   private 
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
     end
 
-		def feed
-		  # This is preliminary. See "Following users" for the full implementation.
-		  Micropost.where("user_id = ?", id)
-		end
 end
